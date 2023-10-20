ddddc6aa6141c212976b78f0a709ccf8610efc62
Sarthak Mishra <sartmis1@in.ibm.com>
2023-10-20 13:03:40 +0530

    updated req file and combined script

--- repo/combined-script.bat
+++ repo/combined-script.bat
@@ -2,14 +2,14 @@
 setlocal enabledelayedexpansion
 
 :: Define the list of commands to run
-set commands[0]=python -u train.py ETTh1 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[1]=python -u train.py ETTh2 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[2]=python -u train.py ETTm1 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[3]=python -u train.py ETTh1 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[4]=python -u train.py ETTh2 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[5]=python -u train.py ETTm1 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[6]=python -u train.py electricity forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
-set commands[7]=python -u train.py electricity forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[0]=python -u train.py ETTh1 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[1]=python -u train.py ETTh2 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[2]=python -u train.py ETTm1 forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[3]=python -u train.py ETTh1 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[4]=python -u train.py ETTh2 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[5]=python -u train.py ETTm1 forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[6]=python -u train.py electricity forecast_univar --loader forecast_csv_univar --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
+set commands[7]=python -u train.py electricity forecast_multivar --loader forecast_csv --repr-dims 320 --max-threads 8 --seed 42 --gpu None --eval
 
 :: Specify the output file
 set output_file=command_output.txt
